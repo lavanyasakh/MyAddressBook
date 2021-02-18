54efea544cbdb76ca6a5a890a6be2802a4901ce2
Lavanya <lavanyace929@gmail.com>
2021-02-18 21:24:33 +0530

    Added:Address Book For Creating Multiple Contacts

--- AddressBook.java
+++ AddressBook.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class AddressBook {
 
     private final String firstName;
@@ -18,14 +19,56 @@ public class AddressBook {
         this.zip = zip;
         this.phoneNum = phoneNum;
         this.email = email;
-    }
+    } 
     public String toString() {
         return "\tFirst Name: "+firstName+"\n\tLast Name: "+lastName+"\n\tAddress: "+houseAddress+"\n\tCity: "+city+
                     "\n\tState: "+state+"\n\tZIP: "+zip+"\n\tPhoneNum: "+phoneNum+"\n\temail: "+email;
     }
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book");
-        AddressBook contact = new AddressBook("Lavanya","Sakhamuri","Pattbhipuram","Guntur","AndhraPradesh",522006,"45902338921","hello@gmai.com");
-        System.out.println(contact);
+        Scanner sc = new Scanner(System.in);
+        // Array Declaration
+        AddressBook[] contacts = new AddressBook[1];
+        //Instance variables
+        int entries, zip;
+        String first, last, address, city, state, phone, email;
+        //Asking User for how many entries in Address Book
+        System.out.println("How many new Entries do you require in AddressBook: ");
+        entries = sc.nextInt();
+        // Initialising Array
+        if ( entries > 0)
+            contacts = new AddressBook[entries];
+        else {
+            System.out.println("Should create atleast 1 Entry");
+            System.exit(0);
+        }
+        System.out.println("\t*****Address Book*****\t");
+        for ( int i = 0 ; i < entries ; i++ ) {
+            System.out.println("Entry: "+(i+1));
+            System.out.print("First name? ");
+            first = sc.next();
+            System.out.print("Last name? ");
+            last = sc.next();
+            System.out.print("Address? ");
+            address = sc.next();
+            System.out.print("City? ");
+            city = sc.next();
+            System.out.print("State? ");
+            state = sc.next();
+            System.out.print("ZIP? ");
+            zip = sc.nextInt();
+            System.out.print("Phone? ");
+            phone = sc.next();
+            System.out.print("Email? ");
+            email = sc.next();
+            contacts[i] = new AddressBook(first, last, address, city, state, zip, phone, email);
+        }
+        for ( int i = 0 ; i < entries ; i++ ) {
+            System.out.println("\t*****Address Book*****\t");
+            System.out.println("Entry: "+(i+1));
+            System.out.println(contacts[i]);
+        }
     }
 }
+   
+
