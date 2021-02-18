2564837bb358e59637e0f4d82edb9ccc00e99821
Lavanya <lavanyace929@gmail.com>
2021-02-18 21:48:26 +0530

    Added:To Edit The Given Contacts

--- AddressBook.java
+++ AddressBook.java
@@ -31,7 +31,7 @@ public class AddressBook {
         AddressBook[] contacts = new AddressBook[1];
         //Instance variables
         int entries, zip;
-        String first, last, address, city, state, phone, email;
+        String first, last, address, city, state, phone, email, choice;
         //Asking User for how many entries in Address Book
         System.out.println("How many new Entries do you require in AddressBook: ");
         entries = sc.nextInt();
@@ -68,7 +68,44 @@ public class AddressBook {
             System.out.println("Entry: "+(i+1));
             System.out.println(contacts[i]);
         }
+        System.out.println("Do you want to edit any Entries u just created? (y/n)");
+        choice = sc.next();
+        if (choice.equals("Y") || choice.equals("y")) {
+            String name;
+            System.out.println("Please enter the first name of Person u want to edit: ");
+            name = sc.next();
+            for (int i = 0; i < entries; i++) {
+                if (Objects.equals(contacts[i].firstName, name)) {
+                    System.out.print("First name? ");
+                    first = sc.next();
+                    System.out.print("Last name? ");
+                    last = sc.next();
+                    System.out.print("Address? ");
+                    address = sc.next();
+                    System.out.print("City? ");
+                    city = sc.next();
+                    System.out.print("State? ");
+                    state = sc.next();
+                    System.out.print("ZIP? ");
+                    zip = sc.nextInt();
+                    System.out.print("Phone? ");
+                    phone = sc.next();
+                    System.out.print("Email? ");
+                    email = sc.next();
+                    contacts[i] = new AddressBook(first, last, address, city, state, zip, phone, email);
+                } else
+                    System.out.println("Person Not found at Entry " + (i + 1) + " with Person FirstName: " + name);
+            }
+            for (int i = 0; i < entries; i++) {
+                System.out.println("\t*****Address Book*****\t");
+                System.out.println("(Edited)Entry: " + (i + 1));
+                System.out.println(contacts[i]);
+            }
+        }
+        else
+            System.out.println("Thank You(:");
     }
 }
+
    
 
